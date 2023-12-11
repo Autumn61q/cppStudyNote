752786639cb0b1a03247094f7d590e84cecfa21e
Autumn61q <144235641+Autumn61q@users.noreply.github.com>
2023-12-11 17:43:16 +0800

    Create main.cpp

---
+++ 2048/main.cpp
@@ -0,0 +1,14 @@
+#include "mainwindow.h"
+
+#include <QApplication>
+
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    MainWindow w;
+    w.show();
+    w.setFixedSize(600,800);
+    w.setWindowTitle("2048");
+    return a.exec();
+}
